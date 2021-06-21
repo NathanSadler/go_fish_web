2dd6efaeccb35c6e1e30b1b4e64d3559ab13122b
Nathan Sadler <nssadler@my.waketech.edu>
2021-06-21 14:44:05 -0400

    add add_card_to_hand method to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -5,4 +5,12 @@ class Player
     @hand = []
     @score = 0
   end
+
+  def add_card_to_hand(card)
+    if(card.is_a?(Array))
+      hand.concat(card)
+    else
+      hand.push(card)
+    end
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../lib/player'
 
 describe 'Player' do
+  let(:player) {Player.new}
   context 'initialize' do
-    let(:test_player) {Player.new}
     it('creates a player with a specified name') do
       player = Player.new('John Doe')
       expect(player.name).to(eq('John Doe'))
@@ -12,10 +12,29 @@ describe 'Player' do
       expect(player.name).to(eq('Player'))
     end
     it("starts the player without any cards") do
-      expect(test_player.hand).to(eq([]))
+      expect(player.hand).to(eq([]))
     end
     it("starts the player with a score of 0") do
-      expect(test_player.score).to(eq(0))
+      expect(player.score).to(eq(0))
+    end
+  end
+
+  context('#add_card_to_hand') do
+    it("adds a card to the player's hand") do
+      test_card = Card.new("7", "H")
+      player.add_card_to_hand(test_card)
+      expect(player.hand).to(eq([test_card]))
+    end
+    it("can add multiple cards to the player's hand") do
+      test_cards = [Card.new("9", "C"), Card.new("K", "S")]
+      player.add_card_to_hand(test_cards)
+      expect(player.hand).to(eq(test_cards))
+    end
+    it("does not just replace the player's hand") do
+      player.add_card_to_hand(Card.new("9", "D"))
+      test_card = Card.new("J", "C")
+      player.add_card_to_hand(test_card)
+      expect(player.hand).to(eq([Card.new( "9",  "D"), test_card]))
     end
   end
 end
