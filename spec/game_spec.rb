eaaa8cc8847577b3d776afb4452d9371eb6a20b3
Nathan Sadler <nssadler@my.waketech.edu>
2021-06-25 12:03:41 -0400

    remove tests for move_turn_pointer since it doesn't exist anymore

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -22,28 +22,8 @@ describe "Game" do
       game.add_player(player)
       expect(game.players).to(eq([player]))
     end
-    it "sets the turn_pointer to 0 if they are the first player" do
-      game = Game.new
-      game.add_player(Player.new)
-      expect(game.move_pointer).to(eq(0))
-    end
   end
 
-
-  context '.move_turn_pointer' do
-    before(:each) do
-      [1, 2, 3].each {|time| game.add_player(Player.new("Player #{time}"))}
-    end
-
-    it("moves to the next player in the players array") do
-      game.move_turn_pointer
-      expect(game.active_player).to(eq(game.players[1]))
-    end
-    it("doesn't do anything if the move_pointer is nil") do
-      3.times {game.move_turn_pointer}
-      expect(game.active_player).to(eq(game.players[0]))
-    end
-  end
 
   context '.take_turn' do
     before(:each) do
